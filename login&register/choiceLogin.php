9c5d8ee82b7cdcf8408f7b46746451bb9e1fe217
eviw817 <evelien.derudder@gmail.com>
2024-04-16 15:46:07 +0200

    started on login choice

---
+++ login&register/choiceLogin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login_registerStylesheet.css">
+    <title>Choice Login</title>
+</head>
+<body>
+    <header class="header">
+
+        <h1>Choose your role:</h1> 
+        
+        <div class="images">
+            <img src="User.png" alt="User">
+            <img src="Manager.png" alt="Manager">
+            <img src="Admin.png" alt="Admin">
+        </div>
+        
+    </header>
+    
+</body>
+</html>
