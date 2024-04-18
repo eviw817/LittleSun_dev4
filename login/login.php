6ee848c32cab458993aeff1e78c68e1085f116e2
eviw817 <evelien.derudder@gmail.com>
2024-04-18 18:41:01 +0200

    logins finised, to be reviewed

---
+++ login/login.php
@@ -0,0 +1,82 @@
+<?php
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . "../classes/Db.php");
+
+    function getAuthorizationDetailsByUsername($pUsername){
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("SELECT username, password, role FROM users WHERE username = :tUsername");
+        $statement->execute([":tUsername" => $pUsername]);
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    $error = null;
+    
+    if(!(isset($_POST) && empty($_POST["username"]) && empty($_POST["password"]))){
+        $user = getAuthorizationDetailsByUsername($_POST["username"]);
+
+        if(!$user){
+            $error = "User does not exist!";
+        }else if($user["password"] != $_POST["password"]){
+            $error = "Credentials do not match!";
+        } else {
+            $baseTag = '<meta http-equiv="refresh" content="0; url=';
+            switch ($user["role"] ) {
+                case "admin":
+                    $redirectTag = $baseTag . '../admin/adminDashboard.php" />';
+                    break;
+                case "manager":
+                    $redirectTag = $baseTag . '../manager/managerDashboard.php" />';
+                    break;
+                case "user":
+                    $redirectTag = $baseTag . '../user/userDashboard.php" />';
+                    break;
+            }
+        }
+    }
+
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        if(isset($redirectTag)){
+            echo $redirectTag;
+        }
+    ?>
+    <link rel="stylesheet" href="loginStylesheet.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+    <title>Login</title>
+</head>
+
+<body>
+    <p><?php if(!empty($error)){echo $error;} ?>
+    <form action="" method="post">
+        <h2>Welcome, please log in</h2>
+
+        <div class="full__form">
+            <div class="form__field">
+                <label for="Username">Username</label>
+                <input id="username" type="text" name="username">
+            </div>
+            <div class="form__field">
+                <label for="Password">Password</label>
+                <input type="password" name="password">
+            </div>
+
+            <div class="form__field">
+                <input type="submit" value="Log in" class="input__button">
+                <div class="checkbox">
+                    <input type="checkbox" id="rememberMe"><label for="rememberMe" class="label__inline">Remember me</label>
+                </div>
+            </div>
+        </div>
+    </form>
+
+</body>
+
+</html>
