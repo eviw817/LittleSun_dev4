28271aeae1153ce0f0c18006e720780735e26b4c
eviw817 <evelien.derudder@gmail.com>
2024-05-05 12:47:53 +0200

    minor fix

--- pages/manager/users/userInfo.php
+++ pages/manager/users/userInfo.php
@@ -1,8 +1,6 @@
 <?php
     include_once(__DIR__ . DIRECTORY_SEPARATOR . "../../../classes/Db.php");
     include_once(__DIR__ . DIRECTORY_SEPARATOR . "../../../classes/users/User.php");
-
-    $userName = new User($_POST["username"], $_POST["email"], $_POST["password"], $_POST["role"], $_POST["location"], $_POST["firstName"], $_POST["lastName"]);
     
 ?>
 
@@ -21,13 +19,14 @@
 <?php include_once("../../../components/header2.inc.php"); ?>
     <h1 class="title">Users</h1>
     <div class="inline">
-    <?php foreach($userName->getName()as $user) : ?> 
+    <?php foreach($userName->getName() as $user) : ?> 
         <div class="flex">
             <a href="userId.php?id=<?php echo $user['id']; ?>" class="user_detail">
                 <p><?php echo $user['firstName'] . " " . $user['lastName'] ; ?></p>
             </a>
             <img width="4.4.375rem" src="<?php echo $user["image"]; ?>"></img>
         </div>
+
     <?php endforeach; ?>
     </div>
     <div class="button-container">
