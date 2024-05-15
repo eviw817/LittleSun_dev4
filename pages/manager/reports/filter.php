42f0f0895200153cebaa1d418fe0aa5316aa94e7
HeliaDonad <r0936750@student.thomasmore.be>
2024-05-15 18:17:29 +0200

    mapjes toevoegen filter

---
+++ pages/manager/reports/filter.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reports</title>
+    <link rel="stylesheet" href="../../../reset.css">
+    <link rel="stylesheet" href="../../../shared.css">
+    <link rel="stylesheet" href="./filter.css">
+</head>
+<body>
+<?php include_once("../../../components/headerManager.inc.php"); ?>
+
+</body>
+</html>
