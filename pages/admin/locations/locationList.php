8c753e610c112db990dd6c28d9c3cacef7b2f5b3
eviw817 <evelien.derudder@gmail.com>
2024-05-22 03:10:02 +0200

    fix

--- pages/admin/locations/locationList.php
+++ pages/admin/locations/locationList.php
@@ -2,14 +2,11 @@
 session_start();
 include_once(__DIR__ . DIRECTORY_SEPARATOR . "../../../classes/Db.php");
 include_once(__DIR__ . DIRECTORY_SEPARATOR . "../../../classes/Location.php");
-include_once(__DIR__ . DIRECTORY_SEPARATOR . "../../../classes/users/Admin.php");
 
 if (!isset($_SESSION['id'])) {
     header("Location: login.php");
     exit();
 }
-
-$admin = Admin::getAdmin($_SESSION['id']);
 
 ?><!DOCTYPE html>
 <html lang="en">
