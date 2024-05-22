5f1d0d28a95f5bf232cf4c1e26fcbd28f199c204
eviw817 <evelien.derudder@gmail.com>
2024-05-22 06:39:52 +0200

    fix

--- pages/manager/users/userId.php
+++ pages/manager/users/userId.php
@@ -66,7 +66,7 @@ if (isset($_GET['id'])) {
             <?php else : ?>
                 <p>No tasks assigned</p>
             <?php endif; ?>
-            <a class="edit" href='./taskAdd.php?id=<?php echo $user['id']; ?>'>Add Task</a>
+            <a class="edit" href='./taskAdd.php?id=<?php echo $_GET['id']; ?>'>Add Task</a>
         </div>
 
     <?php else : ?>
